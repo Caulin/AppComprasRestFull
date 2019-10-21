853fff4d36177ff6cc21b8603a4472fb807e239b
Carlos Henrique dos Santos <carlos.caulin36@gmail.com>
2019-10-20 23:19:21 -0300

    alteracoes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,38 @@
 'use strict';
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
+
+//retira essas linhas para html bruto
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+///
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Shopping", version: "0.0.1"
     });
 });
+const put = router.put('/:id', (req, res, next) => {
+    const id = req.params.id;
+    res.status(200).send({ id: id, item: req.body });
+});
+
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+const del = router.delete('/', (req, res, next) => {
+    res.status(201).send(req.body);
+});
+
 
 app.use('/', route);
+app.use('/produtos', create);
+app.use('/produtos', put);
+app.use('/produtos', del);
+
+
+
 
 module.exports = app;
