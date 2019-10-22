bcdcc1813037e9ee2a327db0f7f24795fdf9798c
Carlos Henrique dos Santos <carlos.caulin36@gmail.com>
2019-10-21 22:45:32 -0300

    conexao ao bd

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
+//conecta ao banco
+mongoose.connect('mongodb://web2:web123456@ds035787.mlab.com:35787/web2', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-//Carrega as rotas
-const indexRoutes =  require('./routers/indexRouter');
+//carrega as rotas
+const indexRoutes = require('./routers/indexRouter');
 const produtoRoutes = require('./routers/produtoRouter');
-//
+
 //retira essas linhas para html bruto
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
