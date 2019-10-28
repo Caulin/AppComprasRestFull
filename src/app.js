0516dabc5fa66c8e38aa862f99178600fdd3920c
Carlos Henrique dos Santos <carlos.caulin36@gmail.com>
2019-10-27 23:25:24 -0300

    ajustes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const cookieParser = require('cookie-parser')
 const session = require('express-session');
 const passport = require('passport');
 const flash = require('connect-flash');
-
+//const MongoStore = require('connect-mongo')(session);
 //conecta ao banco
 mongoose.connect('mongodb://web2:web123456@ds035787.mlab.com:35787/web2', {
     useNewUrlParser: true,
@@ -34,14 +34,24 @@ const pedidoRouters = require('./routers/pedidoRouter');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({ secret: 'segredo', resave: false, saveUninitialized: false }));
+app.use(session({
+    secret: 'segredo',
+    resave: false,
+    saveUninitialized: false,
+    // store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    // cookie: { maxAge: 5 * 60 }
+}));
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 app.set('views', path.join(__dirname, 'views'));
 //app.engine('.hbs', expressHbs({defaultLayout:'layouts',extname:'.hbs'}));
 app.set('view engine', '.hbs');
-
+// app.use((req,res,next)=>{
+//     res.locals.login  = req.isAuthenticated();
+//     res.locals.session = req.session;
+//     next();
+// });
 app.use('/', indexRoutes);
 app.use('/produtos', produtoRoutes);
 app.use('/clientes', clienteRouters);
